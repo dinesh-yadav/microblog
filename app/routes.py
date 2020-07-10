460cdae5ee257788b3f8261e26d2b2e7ba74e9be
Dinesh Yadav <yadav.din@gmail.com>
2020-07-10 18:01:11 +0530

    chapter 2 templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'hello world'
+	user = {'username': 'Dinesh'}
+	#1 return render_template('index.html',title='Home',user=user)
+	#2 return render_template('index.html',user=user)
+	posts = [
+	    {
+		'author':{'username': 'Tom'},
+		'body': 'Beautiful day in Portland!'
+	    },
+	    {
+		'author':{'username': 'Mike'},
+		'body': 'Avengers movie is cool!!'
+	    }
+	]
+	return render_template('index.html',title='Home',user=user,posts=posts)
