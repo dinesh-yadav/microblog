b879800dddc5f9f352ed8e800cc4749fa253f366
Dinesh Yadav <yadav.din@gmail.com>
2020-07-30 14:42:34 +0530

    Chapter 15: A Better Application Structure

--- app/translate.py
+++ app/translate.py
@@ -1,32 +1,19 @@
 import json
-import uuid
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
+
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in app.config or \
-            not app.config['MS_TRANSLATOR_KEY']:
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    '''auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
-    r = requests.post('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from={}&to={}'.format(source_language,dest_language),headers=auth,json={'Text':text})
-    print(r.status_code)
+    auth = {
+        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
+                     '/Translate?text={}&from={}&to={}'.format(
+                         text, source_language, dest_language),
+                     headers=auth)
     if r.status_code != 200:
         return _('Error: the translation service failed.')
-    return json.loads(r.content.decode('utf-8-sig'))'''
-
-    subscription_key = app.config['MS_TRANSLATOR_KEY']
-    endpoint = 'https://api.cognitive.microsofttranslator.com'
-    path = '/translate?api-version=3.0'
-    params = f'&to={dest_language}'
-    constructed_url = endpoint + path + params
-    headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key,
-        "Ocp-Apim-Subscription-Region":'centralindia',
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-        }
-    body = [{'text': text}]
-    request = requests.post(constructed_url, headers=headers, json=body)
-    response = request.json()
-    return response[0]['translations'][0]['text']
+    return json.loads(r.content.decode('utf-8-sig'))
